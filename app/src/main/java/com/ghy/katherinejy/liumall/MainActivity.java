08063d160b45658aa4e06ba0346ac0f9bf46a685
KatherineJY <kellymayliu@163.com>
2018-11-14 22:49:41 +0800

    nav

--- app/src/main/java/com/ghy/katherinejy/liumall/MainActivity.java
+++ app/src/main/java/com/ghy/katherinejy/liumall/MainActivity.java
@@ -1,13 +1,61 @@
 package com.ghy.katherinejy.liumall;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Fragment[] mFragments;
+    private RadioGroup radioGroup;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private RadioButton rbHome,rbFunc,rbSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        mFragments = new Fragment[3];
+        fragmentManager = getSupportFragmentManager();
+        mFragments[0] = fragmentManager.findFragmentById(R.id.fragment_discover);
+        mFragments[1] = fragmentManager.findFragmentById(R.id.fragment_func);
+        mFragments[2] = fragmentManager.findFragmentById(R.id.fragment_setting);
+
+        fragmentTransaction = fragmentManager.beginTransaction().
+                hide(mFragments[0]).hide(mFragments[1]).hide(mFragments[2]);
+        fragmentTransaction.show(mFragments[0]).commit();
+
+        radioGroup = (RadioGroup)findViewById(R.id.bottomGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                fragmentTransaction = fragmentManager.beginTransaction().
+                        hide(mFragments[0]).hide(mFragments[1]).hide(mFragments[2]);
+
+                switch(checkedId){
+                    case R.id.radioHome:
+                        fragmentTransaction.show(mFragments[0]).commit();
+                        break;
+                    case R.id.radioFunc:
+                        fragmentTransaction.show(mFragments[1]).commit();
+                        break;
+                    case R.id.radioSetting:
+                        fragmentTransaction.show(mFragments[2]).commit();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
     }
 }
